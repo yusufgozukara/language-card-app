33ba418cb7d96c9a8fe3996db94bbb95422c9191
yusufgozukara <yusufgozukara11@gmail.com>
2022-06-29 09:29:36 +0300

    v5

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,21 +1,28 @@
 
+import { useState } from 'react'
 import './Card.css'
 
 const Card = ({name, img, options, index}) => {
 
+    const [langu, setLangu] =useState(true);
+
 
 
   return (
-    <div  className="minicard" key={index}>
-        <img src={img} alt="logoimage" />
-        
+    <div  className="minicard" key={index}  onClick={() => setLangu(!langu)}>
+        {langu ? 
+        <div className='minicard2'>
+        <img className='logoimage' src={img} alt="logoimage" />
         <p>{name}</p>
-        
+        </div>
+        : 
         <ul>
             {options.map((item) => {
                 return <li>{item}</li>
             })}
         </ul>
+        }
+        
     </div>
   )
 }
